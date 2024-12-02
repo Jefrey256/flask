656624488b44b71c18439e251152069644b1d3c4
Jeredy <brunojessie144@gmail.com>
2024-12-01 22:06:08 -0300

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from pico import app
 import os
 
-if __name__ == "main":
-  port = int(os.getenv("PORT"), 500)
-  app.run(host= 0.0.0.0, port = port)
+
+if __name__ == 'main':
+  port = int(os.getenv('PORT'), '5000')
+  app.run(host= '0.0.0.0', port = port)
+
+
+###mport os
